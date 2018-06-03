24ca0449b78b059c1aa0dbb77e70244391064323
xiaomaiyun <923782299@qq.com>
2018-06-03 14:27:17 +0800

    xiaomaiyun

--- src/main/java/com/chenjian/utils/Test.java
+++ src/main/java/com/chenjian/utils/Test.java
@@ -2,9 +2,7 @@ package com.chenjian.utils;
 
 public class Test {
     public static void main(String[] args){
-//        ReadExcelUtils.readExcelFilePath("C:\\Users\\root\\Desktop\\123\\校验表_表结构_标准_模板.xls");
         ReadExcelUtils.readExcelFilePath("C:\\Users\\root\\Desktop\\123\\1.xlsx");
-//        System.out.println(ReadExcelUtils.readExcelFilePath("C:\\Users\\root\\Desktop\\123\\校验表_值域表_模板.xls"));
 
     }
 }
