7e3bc937950491f57cf51e5676b30c06051e2540
xiaomaiyun <923782299@qq.com>
2018-06-03 14:30:10 +0800

    xiaomaiyun

--- src/main/java/com/chenjian/utils/Test.java
+++ src/main/java/com/chenjian/utils/Test.java
@@ -2,7 +2,9 @@ package com.chenjian.utils;
 
 public class Test {
     public static void main(String[] args){
-        ReadExcelUtils.readExcelFilePath("C:\\Users\\root\\Desktop\\123\\1.xlsx");
+
+        String content=ReadExcelUtils.readExcelFilePath("C:\\Users\\root\\Desktop\\123\\1.xlsx");
+        System.out.println(content);
 
     }
 }
